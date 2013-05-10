9f8ed22da0d6303b5a2d113a65c28a0b81e48db6
joe <joe@belgarath>
2013-05-09 19:10:52 -0700

    example from Spring Tutorial now functional

--- webapp/src/main/java/com/seo/webapp/BaseController.java
+++ webapp/src/main/java/com/seo/webapp/BaseController.java
@@ -3,8 +3,10 @@ package com.seo.webapp;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
  
 @Controller
 public class BaseController {
@@ -28,13 +30,27 @@ public class BaseController {
  
 	}
 
+	@RequestMapping(value="/student", method = RequestMethod.GET)
+	public ModelAndView student() {
+		return new ModelAndView("student", "command", new Student()); 
+	}
+
+	@RequestMapping(value = "/addStudent", method = RequestMethod.POST)
+	public String addStudent(@ModelAttribute("SpringWeb")Student student, ModelMap model) {
+		model.addAttribute("name", student.getName());
+		model.addAttribute("age", student.getAge());
+		model.addAttribute("id", student.getId());
+
+		return "result";
+	}
+
 	@RequestMapping(value="/test", method = RequestMethod.GET)
 	public String test(ModelMap model) {
  
 		model.addAttribute("message", "Test completed successfully.");
  
 		//Spring uses InternalResourceViewResolver and return back sample.jsp
-		return "sample";
+		return "result";
  
 	}
  
