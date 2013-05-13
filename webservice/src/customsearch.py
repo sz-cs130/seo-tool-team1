5ce9dd8d021ab3204c5b1b067c7df6550a16876a
cs130 <cs130@ubuntu.(none)>
2013-05-12 23:50:47 -0700

    Implemented parsing of the google custom search result

--- webservice/src/customsearch.py
+++ webservice/src/customsearch.py
@@ -1,6 +1,7 @@
 import json
 import urllib
 import urllib2
+from urlparse import urlparse
 
 def getCustomSearchResult(query, customSearchID):
     apiKey = "AIzaSyDnj-rClZX3lgIvaxjfngLKkdtzLW2cm_Y"
@@ -10,6 +11,22 @@ def getCustomSearchResult(query, customSearchID):
     jsonResult = json.load(urllib2.urlopen(customSearchURL))
     return jsonResult
 
+
+
+def getPageTitle(json, index):
+    return json["items"][index]["title"]
+
+
+def getPageUrl(json, index):
+    return json["items"][index]["link"]
+
+def parseDomain(url):
+    return urlparse(url).netloc
+
+def getPageDomain(json, index):
+    url = getPageUrl(json, index)
+    return parseDomain(url)
+
 def main():
     allSitesID = "017616669739552287748:iyhaybi_-pg"
     shopzillaID = "017616669739552287748:nm7mjo9djsk"
@@ -18,8 +35,10 @@ def main():
     
     searchQuery = "hdtv"
 
-    jsonString = getCustomSearchResult(searchQuery, shopzillaID)
-    print json.dumps(jsonString, sort_keys=True, indent=4, separators=(',', ': ')) 
+    jsonResult = getCustomSearchResult(searchQuery, shopzillaID)
+    
+    print getPageTitle(jsonResult, 0)
+    print getPageDomain(jsonResult, 0)
 
 if __name__ == '__main__':
     main()
