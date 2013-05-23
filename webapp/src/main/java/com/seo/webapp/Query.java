56d9152c2b40c959c17c55bdf48db34cb18d232f
Eric Mallare <ejmallare@gmail.com>
2013-05-23 00:42:05 -0700

    Added mock JSON string data and parsed it. Added SiteMetrics to hold the parsed data. Changed results.jsp to hold the mock data but it is very basic right now

--- webapp/src/main/java/com/seo/webapp/Query.java
+++ webapp/src/main/java/com/seo/webapp/Query.java
@@ -1,8 +1,9 @@
 package com.seo.webapp;
-import org.json.simple.*;
 
+import org.json.simple.*;
 import java.net.*;
 import java.io.*;
+import java.util.*;
 
 
 public class Query {
@@ -23,8 +24,8 @@ public class Query {
 	 ********************************************/
 	private String m_query;
 	private String m_siteToCompare = "shopzilla";
-	String s="[0,{\"1\":{\"2\":{\"3\":{\"4\":[5,{\"6\":7}]}}}}]";
-	
+	private String m_JSONString="{  \"Shopzilla\":    {      \"content\": {        \"keyword_pos\": 0,         \"num_keyword\": 0      },       \"description\": {        \"desc_string\": \"Hey wuddup\",         \"keyword_exists\": false      },       \"heading\": {        \"h1\": {          \"keyword_in_h1tags\": 0,           \"num_h1tags\": 0        },         \"h2\": {          \"keyword_in_h2tags\": 0,           \"num_h2tags\": 0        },         \"h3\": {          \"keyword_in_h3tags\": 0,           \"num_h3tags\": 0        },         \"h4\": {          \"keyword_in_h4tags\": 0,           \"num_h4tags\": 0        },         \"h5\": {          \"keyword_in_h5tags\": 0,           \"num_h5tags\": 0        },         \"h6\": {          \"keyword_in_h6tags\": 0,           \"num_h6tags\": 0        }      },       \"title\": {        \"keyword_exists\": false,         \"title_string\": \"Dudez\"      }    },  \"Ebay\":    {      \"content\": {        \"keyword_pos\": 1,         \"num_keyword\": 1      },       \"description\": {        \"desc_string\": \"OMG\",         \"keyword_exists\": false      },       \"heading\": {        \"h1\": {          \"keyword_in_h1tags\": 1,           \"num_h1tags\": 1        },         \"h2\": {          \"keyword_in_h2tags\": 1,           \"num_h2tags\": 1        },         \"h3\": {          \"keyword_in_h3tags\": 1,           \"num_h3tags\": 1        },         \"h4\": {          \"keyword_in_h4tags\": 1,           \"num_h4tags\": 1        },         \"h5\": {          \"keyword_in_h5tags\": 1,           \"num_h5tags\": 1        },         \"h6\": {          \"keyword_in_h6tags\": 1,           \"num_h6tags\": 1        }      },       \"title\": {        \"keyword_exists\": false,         \"title_string\": \"Sparta\"      }    },  \"Amazon\":    {      \"content\": {        \"keyword_pos\": 2,         \"num_keyword\": 2      },       \"description\": {        \"desc_string\": \"Youtube\",         \"keyword_exists\": false      },       \"heading\": {        \"h1\": {          \"keyword_in_h1tags\": 2,           \"num_h1tags\": 2        },         \"h2\": {          \"keyword_in_h2tags\": 2,           \"num_h2tags\": 2        },         \"h3\": {          \"keyword_in_h3tags\": 2,           \"num_h3tags\": 2        },         \"h4\": {          \"keyword_in_h4tags\": 2,           \"num_h4tags\": 2        },         \"h5\": {          \"keyword_in_h5tags\": 2,           \"num_h5tags\": 2        },         \"h6\": {          \"keyword_in_h6tags\": 2,           \"num_h6tags\": 2        }      },       \"title\": {        \"keyword_exists\": false,         \"title_string\": \"Cracked\"      }    }  \"domain\":[\"Shopzilla\",\"Ebay\",\"Amazon\"]}";
+	private ArrayList<SiteMetrics> m_sites;
 
 	/********************************************
 	 *                                          *
@@ -37,7 +38,7 @@ public class Query {
 	 * @return JSON Object in the form of a string 
 	 * @throws Exception	If connection to webservice cannot be established. 
 	 */
-	public String HTTP_Request() throws Exception {
+	public void HTTP_Request() throws Exception {
 		
 		URI site_uri = new URI(
 					// scheme
@@ -69,15 +70,38 @@ public class Query {
 		String prev       = "";
 		
 		while ( (inputLine = in.readLine()) != null ) {
+			//m_JSONString += inputLine;
 			System.out.println(inputLine);
-			prev = inputLine;
+			//prev = inputLine;
 		}
 		
 		in.close();
-		Object obj=JSONValue.parse(s);
-	  JSONArray array=(JSONArray)obj;
 
-		return prev;
+		m_sites = new ArrayList<SiteMetrics>();
+		setSiteArray(m_JSONString);
+
+		//return prev;
+	}
+
+	public void setSiteArray(String jsonString)
+	{
+		JSONObject jobj = (JSONObject)JSONValue.parse(jsonString);
+		JSONArray domainNames = (JSONArray)jobj.get("domain");
+
+		System.out.println(domainNames.size());
+		for(int i = 0; i < domainNames.size(); i++)
+		{
+			String siteName = domainNames.get(i).toString();
+			JSONObject siteObj = (JSONObject)jobj.get(siteName);
+			siteObj.put("domain", siteName);
+			SiteMetrics  siteMets = new SiteMetrics(siteObj);
+			m_sites.add(siteMets);
+		}
+	}
+
+	public ArrayList<SiteMetrics> getSites()
+	{
+		return m_sites;
 	}
 
 	/**
