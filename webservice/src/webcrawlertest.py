1e9fc640018151d063c68a6fe71e0a2726e922fb
Spencer Brett <spencer.a.brett@gmail.com>
2013-06-02 19:31:51 -0700

    Fixed Errors with title and header tags for the web_crawler. It still has trouble parsing the description meta tag, but it just fails to find it rather than produce an error. It should now be workable in other code, although the data might not entirely be correct.

--- webservice/src/webcrawlertest.py
+++ webservice/src/webcrawlertest.py
@@ -2,6 +2,7 @@
 
 # Set up
 import web_crawler
+from json import dumps
 import unittest # Python's unit test module
 
 class TestCrawlFunctions(unittest.TestCase):
@@ -11,15 +12,15 @@ class TestCrawlFunctions(unittest.TestCase):
     
     def test_shopzilla(self):
         address = "http://www.shopzilla.com/tv/11520000/products"
-        print web_crawler.web_crawl(address, self.keyword)
+        print dumps(web_crawler.web_crawl(address, self.keyword),indent=2,sort_keys=True)
 
     def test_bizrate(self):
         address = "http://www.bizrate.com/television-tv/"
-        print web_crawler.web_crawl(address, self.keyword)
+        print dumps(web_crawler.web_crawl(address, self.keyword),indent=2,sort_keys=True)
 
     def test_topresult(self):
         address = "http://www.hgtv.com/"
-        print web_crawler.web_crawl(address, self.keyword)
-
+        print dumps(web_crawler.web_crawl(address, self.keyword),indent=2,sort_keys=True)
+        
 if __name__ == '__main__':
     unittest.main()
