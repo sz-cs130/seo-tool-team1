2d9adcc0de3b88765b9fbaf6794a65bd2e2526f3
Spencer Brett <spencer.a.brett@gmail.com>
2013-06-04 02:59:52 -0700

    Changed the return value to conform to the updated JSON format. Also, made it a little clearer and added error handling.

--- webservice/src/webservice.py
+++ webservice/src/webservice.py
@@ -1,5 +1,6 @@
 from flask import Flask, request
 from flask.ext.restful import Resource, Api, abort
+import urllib2
 from customsearch import getURLs
 from web_crawler import web_crawl
 from json import dumps
@@ -17,14 +18,17 @@ class SEOTool(Resource):
         return getCrawlerResult(keyword, site)
 
 def getCrawlerResult(keyword, site):
-    finalDict = {'domain': []}
-    urlDict = getURLs(keyword, site)
-
-    for key,value in urlDict.iteritems():
-        finalDict[key] = web_crawl(value, keyword)
-        finalDict['domain'].append(key)
-
-    return finalDict
+    parse_data = {'domain':[]} # A dictionary of scraped data from specific web pages
+    try:
+        search_results = getURLs(keyword, site) # results of custom Google Search using keywords
+        for domain,url in search_results.iteritems():
+            parse_data[domain] = web_crawl(url, keyword)
+            parse_data['domain'].append(domain)
+    except urllib2.HTTPError, err:
+        abort(err.code, message=str(err.reason))
+    except Exception, e:
+        abort(400, message=str(e))
+    return parse_data
 
 api.add_resource(SEOTool, '/<string:keyword>/<string:site>')
 
